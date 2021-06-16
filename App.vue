00b4444774aac9204c4f4496dd430ea63c641b3b
wym <2567403084@qq.com>
2021-06-16 18:01:10 +0800

    123

--- App.vue
+++ App.vue
@@ -4,62 +4,86 @@ export default {
 		// console.log('App Launch');
 		// uni.subscribePush({
 		// 	provider: "unipush",
-  //           success: function(res) {
-  //               console.log("success:" + JSON.stringify(res));
-  //           },
+		//           success: function(res) {
+		//               console.log("success:" + JSON.stringify(res));
+		//           },
 		// 	fail:function(err){
 		// 		console.log(err)
 		// 	}
 		// })
-		plus.push.addEventListener("click",function(message){
+		const clientInfo = plus.push.getClientInfo();
+		console.log(12, clientInfo);
+		this.$api.postapi('/api/admin/upd_user_cid', { cid: clientInfo.clientid, loginId: uni.getStorageSync('loginId') }).then(res => {
 			
-		})
-		plus.push.addEventListener('receive',function(message){
-			console.log(222,message)
-		})
-		const clientInfo = plus.push.getClientInfo()
-		console.log(12,clientInfo)
-		  // const _self = this;
-		  // //监听系统通知栏消息点击事件  
-		  // plus.push.addEventListener('click', function(message) { 
-		  //  //处理点击消息的业务逻辑代码
-		  //   console.log(message);
-		  //       plus.nativeUI.toast('click:'+JSON.stringify(message)); 
-		  //   //plus.nativeUI.toast('push click');  
-		  //   _self._handlePush(message);  
-		  // }, false);  
-		  // //监听接收透传消息事件  
-		  // plus.push.addEventListener('receive', function(message) {  
-		  //   console.log(message);
-		  //       plus.nativeUI.toast('receive:'+JSON.stringify(message)); 
-		  //   //处理透传消息的业务逻辑代码  
-		  //   //plus.nativeUI.toast('push receive');  
-		  //   _self._handlePush(message);  
-		  // }, false);
+		});
+		plus.push.setAutoNotification(true);
+		const _self = this;
+		var sensor_id = '';
+		var type = '';
+		//监听接收透传消息事件
+		plus.push.addEventListener(
+			'receive',
+			function(message) {
+				console.log(111, message);
+				let options = { cover: false, sound: 'system', title: message.title};
+				sensor_id = message.payload.sensor_id;
+				type = message.payload.type;
+				let obj = {
+					sensor_id:id,
+					type:type
+				}
+				plus.push.createMessage(message.content,obj, options);
+				//     plus.nativeUI.toast('receive:'+JSON.stringify(message));
+				// //处理透传消息的业务逻辑代码
+				// plus.nativeUI.toast('push receive');
+				// _self._handlePush(message);
+			},
+			false
+		);
+		//监听系统通知栏消息点击事件
+		plus.push.addEventListener(
+			'click',
+			function(message) {
+				//处理点击消息的业务逻辑代码
+				console.log(222, message);
+				if (message.payload.type == 0) {
+					setTimeout(() => {
+						uni.navigateTo({
+							url: '/pages/Dangerin/Confirmed/Confirmed?id=' + message.payload.sensor_id + '&index=0'
+						});
+					},500)
+				}
+				if (message.payload.type == 1) {
+					setTimeout(() => {
+						uni.redirectTo({
+							url: '/pages/Dangerin/Confirmed/Confirmed?id=' + message.payload.sensor_id + '&index=1'
+						});
+					},500)
+				}
+			},
+			false
+		);
 	},
 	onShow: function() {
-		console.log('App Show');
-		uni.hideTabBar()
+		uni.hideTabBar();
 	},
-	onHide: function() {
-		console.log('App Hide');
-	}
+	onHide: function() {}
 };
 </script>
 
 <style lang="less">
-	page{
-		background: #033785;
+page {
+	background: #033785;
+}
+uni-rich-text {
+	height: auto;
+	text {
+		color: rgb(255, 255, 255) !important;
 	}
-	uni-rich-text{
-		height: auto;
-		text{
-			color: rgb(255,255,255) !important;
-		}
-		span{
-			color: rgb(255,255,255) !important;
-		}
+	span {
+		color: rgb(255, 255, 255) !important;
 	}
+}
 body,
 html {
 	width: 100%;
@@ -114,44 +138,43 @@ uni-switch .uni-switch-input:before {
 	top: 0;
 	height: 100%;
 	margin: 0 auto;
-	background: linear-gradient(360deg, #0D4B88 0%, rgba(1, 49, 97, 0) 100%);
+	background: linear-gradient(360deg, #0d4b88 0%, rgba(1, 49, 97, 0) 100%);
 	box-shadow: 0px -6px 12px rgba(44, 28, 28, 0.08);
 	background-position: top, bottom;
 	background-size: 100% 102rpx;
 	background-repeat: no-repeat;
 }
-.uni-picker-view-wrapper{
-	background: linear-gradient(360deg, #0D4B88 0%, rgba(1, 49, 97, 0) 100%);
+.uni-picker-view-wrapper {
+	background: linear-gradient(360deg, #0d4b88 0%, rgba(1, 49, 97, 0) 100%);
 	box-shadow: 0px -6px 12px rgba(44, 28, 28, 0.08);
 }
 
-.uni-popup{
-	
+.uni-popup {
 }
-.picker-view-two{
-	uni-picker-view-column{
-		.uni-picker-view-indicator{
+.picker-view-two {
+	uni-picker-view-column {
+		.uni-picker-view-indicator {
 			border-top: 2rpx solid rgba(90, 232, 255, 0.5);
 			border-bottom: 2rpx solid rgba(90, 232, 255, 0.5);
-			&::before{
+			&::before {
 				border: none;
 				background: none;
 			}
-			&::after{
+			&::after {
 				border: none;
 				background: none;
-				
 			}
 		}
-		&:nth-child(3),&:nth-child(4){
-			.uni-picker-view-indicator{
+		&:nth-child(3),
+		&:nth-child(4) {
+			.uni-picker-view-indicator {
 				position: relative;
-				&::before{
+				&::before {
 					content: '';
 					border: none;
 					background: none;
 				}
-				&::after{
+				&::after {
 					content: ':';
 					height: auto;
 					border: none;
@@ -165,5 +188,4 @@ uni-switch .uni-switch-input:before {
 		}
 	}
 }
-
 </style>
